cc65a4bb9ef13b15021d3bad904e5bdd3b3e17e6
Benjamin CHARLET <ben.ch.1910@gmail.com>
2017-05-19 23:53:20 +0200

    Update app.js

--- public/angularjs/app.js
+++ public/angularjs/app.js
@@ -7,7 +7,7 @@ The function getSLackProfile should be in the "getSlackUsers.js" file but a mist
 var app = angular.module('myApp', []).controller('getSlackProfile', getSlackProfile);
 
 function getSlackProfile($http) {
-    var token = "xoxp-54295399571-126184120261-164671004707-e5aa1cbb53bd4727a3946cfc46cbc4ca";
+    var token = ""; 
     var urlSlack = "https://slack.com/api/users.profile.get?token="+token+"&user="+"U3Q5E3J7P"+"&pretty=1";
 
     var vm = this;
