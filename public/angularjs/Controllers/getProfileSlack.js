254b3cb71bb8306b0ddfb21ff5a441b25ea570c0
Benjamin CHARLET <ben.ch.1910@gmail.com>
2017-05-19 22:57:12 +0200

    Update getProfileSlack.js

--- public/angularjs/Controllers/getProfileSlack.js
+++ public/angularjs/Controllers/getProfileSlack.js
@@ -1,7 +1,7 @@
 /* THIS FILE DEFINES AN ANGULAR CONTROLLER WHICH GETS THE USER'S INFORMATION ABOUT HIS PROFILE */
 
 var token = ""; // The toke can be found here : https://api.slack.com/custom-integrations/legacy-tokens
-var IDuser = "U3Q5E3J7P"; // This ID can be found here : https://api.slack.com/methods/users.profile.get/test by clicking on the @username
+var IDuser = ""; // This ID can be found here : https://api.slack.com/methods/users.profile.get/test by clicking on the @username
 var urlSlack = "https://slack.com/api/users.profile.get?token="+token+"&user="+IDuser+"&pretty=1";
 
 (function () {
